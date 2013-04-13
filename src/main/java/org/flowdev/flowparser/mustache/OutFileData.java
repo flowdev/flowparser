2b5b42f977ba81fb1d53c0f8945f79296e75fb58
ole108 <ole@bulbuk.de>
2013-04-13 20:31:40 +0200

    Next little step before really using the template enging.

---
+++ src/main/java/org/flowdev/flowparser/mustache/OutFileData.java
@@ -0,0 +1,6 @@
+package org.flowdev.flowparser.mustache;
+
+public class OutFileData {
+    public String fileName;
+    public String fileContent;
+}
--- src/main/java/org/flowdev/flowparser/mustache/OutputFlowFile.java
+++ src/main/java/org/flowdev/flowparser/mustache/OutputFlowFile.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.flowdev.base.Getter;
 import org.flowdev.base.Port;
 import org.flowdev.base.op.Configure;
+import org.flowdev.flowparser.data.Flow;
 import org.flowdev.flowparser.data.FlowFile;
 
 public class OutputFlowFile<T> extends Configure<OutputFlowFileConfig> {
@@ -13,13 +14,11 @@ public class OutputFlowFile<T> extends Configure<OutputFlowFileConfig> {
     }
 
     private final Params<T> params;
-    private final Port<T> inPort = new Port<T>() {
+    private Port<OutFileData> out;
+    private final Port<T> in = new Port<T>() {
 	@Override
 	public void send(T data) {
-	    FlowFile flowFile = params.getFlowFile.get(data);
-	    Map<String, String> roots = getVolatileConfig().roots;
-	    System.out.println("Roots: " + roots.toString());
-	    System.out.println("Flows: " + flowFile.toString());
+	    outputFlowFile(data);
 	}
     };
 
@@ -27,7 +26,33 @@ public class OutputFlowFile<T> extends Configure<OutputFlowFileConfig> {
 	this.params = params;
     }
 
+    private void outputFlowFile(T data) {
+	FlowFile flowFile = params.getFlowFile.get(data);
+	Map<String, String> roots = getVolatileConfig().roots;
+	System.out.println("Roots: " + roots.toString());
+	System.out.println("Flows: " + flowFile.toString());
+
+	for (Map.Entry<String, String> entry : roots.entrySet()) {
+	    outputFlowFileFormatted(entry.getKey(), entry.getValue(), flowFile);
+	}
+    }
+
+    private void outputFlowFileFormatted(String format, String root,
+	    FlowFile flowFile) {
+	for (Flow flow : flowFile.flows) {
+	    outputFlowFormatted(format, root, flow);
+	}
+    }
+
+    private void outputFlowFormatted(String format, String root, Flow flow) {
+
+    }
+
     public Port<T> getIn() {
-	return inPort;
+	return in;
+    }
+
+    public void setOut(Port<OutFileData> out) {
+	this.out = out;
     }
 }
