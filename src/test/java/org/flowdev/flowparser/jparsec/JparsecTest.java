438be275a3a7dd0db2ad73954f87b63f5d633e83
ole108 <ole@bulbuk.de>
2013-12-11 21:53:26 +0100

    Added test implementation for JParsec

--- src/test/java/org/flowdev/flowparser/jparsec/JparsecTest.java
+++ src/test/java/org/flowdev/flowparser/jparsec/JparsecTest.java
@@ -1,12 +1,20 @@
 package org.flowdev.flowparser.jparsec;
 
+import org.codehaus.jparsec.Parser;
+import org.codehaus.jparsec.Parsers;
+import org.codehaus.jparsec.Scanners;
+import org.codehaus.jparsec.Terminals;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class JparsecTest {
+import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
+public class JparsecTest {
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
     }
@@ -21,7 +29,14 @@ public class JparsecTest {
 
     @Test
     public void test() {
-	// fail("Not yet implemented");
+        Terminals operators = Terminals.operators(","); // only one operator supported so far
+        Parser<?> integerTokenizer = Terminals.IntegerLiteral.TOKENIZER;
+        Parser<String> integerSyntacticParser = Terminals.IntegerLiteral.PARSER;
+        Parser<?> ignored = Parsers.or(Scanners.JAVA_BLOCK_COMMENT, Scanners.WHITESPACES);
+        Parser<?> tokenizer = Parsers.or(operators.tokenizer(), integerTokenizer); // tokenizes the operators and integer
+        Parser<List<String>> integers = integerSyntacticParser.sepBy(operators.token(","))
+                .from(tokenizer, ignored.skipMany());
+        assertEquals(Arrays.asList("1", "2", "3"), integers.parse("1, /*this is comment*/2, 3"));
     }
 
 }
